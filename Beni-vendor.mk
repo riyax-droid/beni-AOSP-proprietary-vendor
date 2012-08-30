3d831d1557796f597d94dd89c5c785829ca1afcb
root <root@vishwa-desktop.(none)>
2012-08-30 13:17:58 +0530

    final commit

---
+++ Beni-vendor.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Live wallpaper packages and Themes
+PRODUCT_PACKAGES := \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    librs_jni 
+
+# Inherit some tmobile stuff.
+#$(call inherit-product, vendor/samsung/Beni/tmobile/Android.mk)
+
+# Publish that we support the live wallpaper feature.
+PRODUCT_COPY_FILES := \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml \
+    frameworks/base/data/etc/com.tmobile.software.themes.xml:/system/etc/permissions/com.tmobile.software.themes.xml
+
+# Pick up overlay for features that depend on non-open-source files
+DEVICE_PACKAGE_OVERLAYS := vendor/samsung/Beni/overlay
+
+$(call inherit-product, vendor/samsung/Beni/Beni-vendor-blobs.mk)
